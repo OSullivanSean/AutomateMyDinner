6194be898528b9abd66543d4281f2b21978951e5
sean <sosullivan06@qub.ac.uk>
2018-07-12 11:27:59 +0100

    Initial commit

---
+++ src/main/java/com/sean/AutomateMyDinner/AutomateMyDinnerApplication.java
@@ -0,0 +1,12 @@
+package com.sean.AutomateMyDinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomateMyDinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomateMyDinnerApplication.class, args);
+	}
+}
